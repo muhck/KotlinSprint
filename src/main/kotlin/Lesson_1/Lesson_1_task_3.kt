2694d2d298bfef42fc851547856e74d08eddc022
muhck <dimamic@yandex.ru>
2023-09-11 23:12:03 +0400

    KS-1-3

---
+++ src/main/kotlin/Lesson_1/Lesson_1_task_3.kt
@@ -0,0 +1,19 @@
+package Lesson_1
+
+fun main () {
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    //println("$hour:$minute")
+
+    print(hour)
+    print(":")
+    print(minute)
+}
