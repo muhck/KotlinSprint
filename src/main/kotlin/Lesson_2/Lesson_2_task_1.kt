c660e7c5ade13cc6002b699f0e86d80bba394e82
muhck <dimamic@yandex.ru>
2023-09-12 23:52:42 +0400

    KS-2-1 посчитали средний балл школьников

---
+++ src/main/kotlin/Lesson_2/Lesson_2_task_1.kt
@@ -0,0 +1,11 @@
+package Lesson_2
+
+fun main() {
+    val petya = 3
+    val masha = 4
+    val dima = 3
+    val ivan = 5
+    val averageScore: Float = (petya + masha + dima + ivan) / 4f
+
+    println("Средний балл 7 А класса: $averageScore")
+}
