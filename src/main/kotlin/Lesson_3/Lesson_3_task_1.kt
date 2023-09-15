f455430ddcc2dc895b0e9a5814e02cb094070b08
muhck <dimamic@yandex.ru>
2023-09-15 23:04:21 +0400

    KS-3-1 вывели строку приветствия

---
+++ src/main/kotlin/Lesson_3/Lesson_3_task_1.kt
@@ -0,0 +1,11 @@
+package Lesson_3
+
+fun main() {
+    val greetingDay = "Добрый день"
+    val greetingNight = "Добрый вечер"
+    val space = " "
+    var userName = "Dmitry"
+
+    println(greetingDay + space + userName)
+    println("$greetingNight $userName")
+}
