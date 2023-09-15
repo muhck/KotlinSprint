f8c932d7d3a5dcc255784950f82fb68ad66303bd
muhck <dimamic@yandex.ru>
2023-09-15 23:37:53 +0400

    KS-3-2 вывели ФИО и возраст

---
+++ src/main/kotlin/Lesson_3/Lesson_3_task_2.kt
@@ -0,0 +1,15 @@
+package Lesson_3
+
+fun main() {
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = "20лет"
+
+    println("$surname $name $patronymic, $age")
+
+    surname = "Сидорова"
+    age = "22года"
+
+    println("$surname $name $patronymic, $age")
+}
