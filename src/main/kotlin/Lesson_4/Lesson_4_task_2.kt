0dd6b8204e763cebb80f6700f3cad8ecf81cb140
muhck <dimamic@yandex.ru>
2023-09-20 20:56:38 +0400

    KS-4-2 вывели сравнение вес и объем грузов

---
+++ src/main/kotlin/Lesson_4/Lesson_4_task_2.kt
@@ -0,0 +1,30 @@
+package Lesson_4
+
+fun main() {
+    val minWeightAverage = 35
+    val maxWeightAverage = 100
+    val volumeAverage = 100
+
+    val weightOfTheFirstLoad = 20
+    val weightOfTheFirstVolume = 80
+
+    println(
+        "Груз с весом $weightOfTheFirstLoad кг и объемом $weightOfTheFirstVolume л соответствует категории" +
+                " 'Average': ${
+                    weightOfTheFirstLoad >= minWeightAverage && weightOfTheFirstLoad <= maxWeightAverage &&
+                            weightOfTheFirstVolume < volumeAverage
+                }"
+    )
+
+    val weightOfTheSecondLoad = 50
+    val weightOfTheSecondVolume = 100
+
+    println(
+        "Груз с весом $weightOfTheFirstLoad кг и объемом $weightOfTheFirstVolume л соответствует категории" +
+                " 'Average': ${
+                    weightOfTheSecondLoad >= minWeightAverage && weightOfTheSecondLoad <= maxWeightAverage &&
+                            weightOfTheSecondVolume < volumeAverage
+                }"
+    )
+
+}
