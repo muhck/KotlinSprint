eb438272a0e9ed8080296b5aeb9c8a652af25906
muhck <dimamic@yandex.ru>
2023-09-20 22:08:24 +0400

    KS-4-1 исправил константу

--- src/main/kotlin/Lesson_4/Lesson_4_task_1.kt
+++ src/main/kotlin/Lesson_4/Lesson_4_task_1.kt
@@ -1,11 +1,11 @@
 package Lesson_4
 
-const val numberOfTables = 13
+const val Number_of_Tables = 13
 
 fun main() {
     var reservedTablesToday = 13
     var reservedTablesTomorrow = 9
 
-    println("Доступность столиков на сегодня:${reservedTablesToday < numberOfTables}\n" +
-            "Доступность столиков на завтра:${reservedTablesTomorrow < numberOfTables}")
+    println("Доступность столиков на сегодня:${reservedTablesToday < Number_of_Tables}\n" +
+            "Доступность столиков на завтра:${reservedTablesTomorrow < Number_of_Tables}")
 }
