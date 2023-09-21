47230a7cb7e8f3f3a98f05d67db3ee6ddc60e6c6
muhck <dimamic@yandex.ru>
2023-09-21 22:01:50 +0400

    KS-4-5 написали возможность отправки корабля в плавание

---
+++ src/main/kotlin/Lesson_4/Lesson_4_task_5.kt
@@ -0,0 +1,19 @@
+package Lesson_4
+
+fun main() {
+    print("Имеет ли корабль повреждения: ")
+    val isDamaged = readlnOrNull().toBoolean()
+    print("Текущий состав экипажа: ")
+    val crewComposition = readln().toInt()
+    print("Количество ящиков с провизией на борту: ")
+    val provisionsOnBoard = readln().toInt()
+    print("Благоприятна ли погода: ")
+    val isSunny = readlnOrNull().toBoolean()
+
+    if (isDamaged == false && crewComposition >= 55 && crewComposition <= 70 && provisionsOnBoard > 50)
+        println("Научно-исследовательский корабль может приступить к долгосрочному плаванию")
+    else if (isDamaged == true && crewComposition == 70 && provisionsOnBoard > 50 && isSunny == true)
+        println("Научно-исследовательский корабль может приступить к долгосрочному плаванию")
+    else
+        println("Научно-исследовательский корабль не может приступить к долгосрочному плаванию")
+}
