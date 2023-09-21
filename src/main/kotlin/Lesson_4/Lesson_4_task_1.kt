a15088a3e0b6a9101baaef0b5fea48dd1e60e2c1
muhck <120320336+muhck@users.noreply.github.com>
2023-09-21 12:36:21 +0400

    KS-4-1 изменил название константы в строчке println

--- src/main/kotlin/Lesson_4/Lesson_4_task_1.kt
+++ src/main/kotlin/Lesson_4/Lesson_4_task_1.kt
@@ -6,6 +6,6 @@ fun main() {
     var reservedTablesToday = 13
     var reservedTablesTomorrow = 9
 
-    println("Доступность столиков на сегодня:${reservedTablesToday < Number_of_Tables}\n" +
-            "Доступность столиков на завтра:${reservedTablesTomorrow < Number_of_Tables}")
+    println("Доступность столиков на сегодня:${reservedTablesToday < NUMBER_OF_TABLES}\n" +
+            "Доступность столиков на завтра:${reservedTablesTomorrow < NUMBER_OF_TABLES}")
 }
