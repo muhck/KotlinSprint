f8fc9343c82170763c82a4c40c77bd0c1289e801
muhck <dimamic@yandex.ru>
2023-09-25 23:41:35 +0400

    KS-5-2 написал проверку совершеннолетия по году

--- src/main/kotlin/Lesson_5/Lesson_5_task_2.kt
+++ src/main/kotlin/Lesson_5/Lesson_5_task_2.kt
@@ -1,2 +1,16 @@
 package Lesson_5
 
+import java.util.*
+
+val DATE = Calendar.getInstance()
+val AGE_OF_MAGORITY = DATE.get(Calendar.YEAR) - 18
+
+fun main() {
+    print("Введите год рождения: ")
+    val userYearOfBirth = readLine()!!.toInt()
+
+    val resultText = if (userYearOfBirth <= AGE_OF_MAGORITY) "Показать экран со скрытым контентом"
+    else "Вернуться на главный экран"
+
+    println(resultText)
+}
