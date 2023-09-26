a05af79144780ab2b1dfafc50900953c70230d4b
muhck <dimamic@yandex.ru>
2023-09-27 03:50:51 +0400

    KS-5-5 написал код рулетки

--- src/main/kotlin/Lesson_5/Lesson_5_task_5.kt
+++ src/main/kotlin/Lesson_5/Lesson_5_task_5.kt
@@ -1,2 +1,29 @@
 package Lesson_5
 
+fun main() {
+    print("Испытай удачу! Угадай первое число от 1 до 100: ")
+    val firstNumberEntered = readln().toInt()
+    print("Испытай удачу! Угадай второе число от 1 до 100: $firstNumberEntered, ")
+    val secondNumberEntered = readln().toInt()
+
+    val firstCorrectNumber = (1..100).random()
+    var secondCorrectNumber = (1..100).random()
+
+    while (secondCorrectNumber == firstCorrectNumber) secondCorrectNumber = (1..100).random()
+
+    print(
+        "Правильный ответ: $firstCorrectNumber и $secondCorrectNumber. Нажмите клавишу \"ENTER\" чтобы продолжить"
+    )
+    val useEnter = readln()
+
+    if (firstNumberEntered == firstCorrectNumber && secondNumberEntered == secondCorrectNumber)
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if (firstNumberEntered == secondCorrectNumber && secondNumberEntered == firstCorrectNumber)
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if (
+        firstNumberEntered == firstCorrectNumber || firstNumberEntered == secondCorrectNumber ||
+        secondNumberEntered == firstCorrectNumber || secondNumberEntered == secondCorrectNumber
+    )
+        println("Вы выиграли утешительный приз!")
+    else println("Неудача! Крутите барабан!")
+}
