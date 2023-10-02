fc21f18bda1317c357bc64e022eec654c15f7678
muhck <dimamic@yandex.ru>
2023-10-02 21:35:36 +0400

    KS-6-4 угадываем число от 1 до 9

--- src/main/kotlin/Lesson_6/Lesson_6_task_4.kt
+++ src/main/kotlin/Lesson_6/Lesson_6_task_4.kt
@@ -1,2 +1,16 @@
 package Lesson_6
 
+fun main() {
+    val numberToWin = (1..9).random()
+    var attempts = 5
+
+    print("Угадай число от 1 до 9. У тебя $attempts попыток. Введи число: ")
+    var enteredNumber = readln().toInt()
+
+    while (attempts > 1 && enteredNumber != numberToWin) {
+        println("Не угадал! Осталось попыток: ${--attempts}")
+        enteredNumber = readln().toInt()
+    }
+    if (enteredNumber == numberToWin) println("Это была великолепная игра!")
+    else println("Было задано число $numberToWin")
+}
