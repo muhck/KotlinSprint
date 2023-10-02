471f4cfd164ed014ab19a83990ba0e808660ed5b
muhck <dimamic@yandex.ru>
2023-10-02 20:10:08 +0400

    KS-6-3 написал код для таймера с обратным отсчетом

--- src/main/kotlin/Lesson_6/Lesson_6_task_3.kt
+++ src/main/kotlin/Lesson_6/Lesson_6_task_3.kt
@@ -1,2 +1,15 @@
 package Lesson_6
 
+fun main() {
+    print("Введите время в секундах для активации таймера: ")
+    var enteredTime = readln().toInt()
+
+    while (enteredTime > 0) {
+        if (enteredTime >= 5) println("Осталось ${enteredTime--} секунд")
+        else if (enteredTime > 1) println("Осталось ${enteredTime--} секунды")
+        else println("Осталось ${enteredTime--} секунда")
+        Thread.sleep(1000)
+    }
+
+    println("Время вышло.")
+}
