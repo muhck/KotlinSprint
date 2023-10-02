2f22b526e72de08b762fc532f15678f08ef00606
muhck <dimamic@yandex.ru>
2023-10-02 19:54:20 +0400

    KS-6-2 написал код для таймера

--- src/main/kotlin/Lesson_6/Lesson_6_task_2.kt
+++ src/main/kotlin/Lesson_6/Lesson_6_task_2.kt
@@ -1,2 +1,10 @@
 package Lesson_6
 
+fun main() {
+    print("Введите время в секундах для активации таймера: ")
+    val enteredTime = readln().toLong()
+    val enteredTimeInMilliseconds = enteredTime * 1000
+    Thread.sleep(enteredTimeInMilliseconds)
+
+    println("Прошло $enteredTime секунд")
+}
