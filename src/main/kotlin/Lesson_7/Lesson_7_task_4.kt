f8472d4306a8e219aa897b83217311ac46c6e1ba
muhck <dimamic@yandex.ru>
2023-10-05 02:21:03 +0400

    KS-7-4 написал таймер

--- src/main/kotlin/Lesson_7/Lesson_7_task_4.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_task_4.kt
@@ -1,2 +1,15 @@
 package Lesson_7
 
+fun main() {
+    print("Введите количество секунд которые нужно засечь: ")
+    val enteredNumber = readln().toInt()
+
+    for (i in enteredNumber downTo 0) {
+        if (i == 0) {
+            print("Время вышло!")
+            return
+        }
+        println("Осталось секунд: $i")
+        Thread.sleep(1000)
+    }
+}
