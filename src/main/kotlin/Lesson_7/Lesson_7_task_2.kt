98acc1b21e0c2ea3f0fd3aefd3f7f2529b76a739
muhck <dimamic@yandex.ru>
2023-10-05 01:33:35 +0400

    KS-7-2 авторизация входа

--- src/main/kotlin/Lesson_7/Lesson_7_task_2.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_task_2.kt
@@ -1,2 +1,24 @@
 package Lesson_7
 
+fun main() {
+    var activationCode = (1000 until 10000).random()
+
+    println("Ваш код авторизации: $activationCode")
+    Thread.sleep(1000)
+
+    print("Введите код для авторизации: ")
+    val enteredCode = readln().toInt()
+
+    while (enteredCode != activationCode) {
+        println("Код введен неверно.")
+        activationCode = (1000 until 10000).random()
+
+        println("Ваш новый код авторизации: $activationCode")
+        Thread.sleep(1000)
+
+        print("Введите код для авторизации: ")
+        val enteredCode = readln().toInt()
+        if (enteredCode == activationCode) break
+    }
+    println("Доброй пожаловать!")
+}
