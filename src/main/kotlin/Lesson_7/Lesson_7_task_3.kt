dddd3206a8e241e76d5738551ce1132c1753de8e
muhck <dimamic@yandex.ru>
2023-10-05 02:10:21 +0400

    KS-7-3 вывод четного числа

--- src/main/kotlin/Lesson_7/Lesson_7_task_3.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_task_3.kt
@@ -1,2 +1,9 @@
 package Lesson_7
 
+fun main() {
+    print("Введите число, от которого будем выводить все чётные числа: ")
+    val enteredNumber = readln().toInt()
+
+    for (i in 1..enteredNumber)
+        if (i % 2 == 0) println(i)
+}
