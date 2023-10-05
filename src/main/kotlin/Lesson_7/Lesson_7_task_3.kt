65ac86d9c3aa8799fa8240c83b0165e2732bda42
muhck <120320336+muhck@users.noreply.github.com>
2023-10-06 03:03:38 +0400

    Использовал step 2

--- src/main/kotlin/Lesson_7/Lesson_7_task_3.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_task_3.kt
@@ -4,6 +4,6 @@ fun main() {
     print("Введите число, от которого будем выводить все чётные числа: ")
     val enteredNumber = readln().toInt()
 
-    for (i in 1..enteredNumber)
-        if (i % 2 == 0) println(i)
+    for (i in 0..enteredNumber step 2)
+    println(i)
 }
