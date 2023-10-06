f0fcb14c263cf3343a86a61fc13b8e5822ae0140
muhck <dimamic@yandex.ru>
2023-10-06 18:04:19 +0400

    KS-7-4 убрал условие

--- src/main/kotlin/Lesson_7/Lesson_7_task_4.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_task_4.kt
@@ -4,12 +4,9 @@ fun main() {
     print("Введите количество секунд которые нужно засечь: ")
     val enteredNumber = readln().toInt()
 
-    for (i in enteredNumber downTo 0) {
-        if (i == 0) {
-            print("Время вышло!")
-            return
-        }
+    for (i in enteredNumber downTo 1) {
         println("Осталось секунд: $i")
         Thread.sleep(1000)
     }
+    print("Время вышло!")
 }
