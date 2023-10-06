452e8b2864db5e7570520a7171fffe4dd006903e
muhck <120320336+muhck@users.noreply.github.com>
2023-10-07 03:42:47 +0400

    KS-7-5 написал генератор пароля (#43)

--- src/main/kotlin/Lesson_7/Lesson_7_task_5.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_task_5.kt
@@ -1,2 +1,11 @@
 package Lesson_7
 
+fun main() {
+    print("Чтобы задать длину сгенерированного пароля, введите необходимое количество символов: ")
+    val enteredNumber = readln().toInt()
+
+    val password = CharArray(enteredNumber) {
+        listOf('a'..'z', 'A'..'Z', '0'..'9').random().random()
+    }
+    println(password)
+}
