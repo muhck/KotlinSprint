e90ad013bb7134c641834a4c78d3a86a0f5c021f
muhck <dimamic@yandex.ru>
2023-10-06 17:40:18 +0400

    KS-7-2 исправил "while" на "do while"

--- src/main/kotlin/Lesson_7/Lesson_7_task_2.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_task_2.kt
@@ -3,22 +3,35 @@ package Lesson_7
 fun main() {
     var activationCode = (1000 until 10000).random()
 
-    println("Ваш код авторизации: $activationCode")
+    println("Ваш код для активации: $activationCode")
     Thread.sleep(1000)
 
-    print("Введите код для авторизации: ")
-    val enteredCode = readln().toInt()
+    print("Введите код для активации: ")
+    var enteredCode = readln().toInt()
 
     while (enteredCode != activationCode) {
-        println("Код введен неверно.")
-        activationCode = (1000 until 10000).random()
-
-        println("Ваш новый код авторизации: $activationCode")
+        println("Код введен неверно!")
         Thread.sleep(1000)
 
-        print("Введите код для авторизации: ")
-        val enteredCode = readln().toInt()
-        if (enteredCode == activationCode) break
+        activationCode = (1000 until 10000).random()
+        println("Ваш новый код для активации: $activationCode")
+        Thread.sleep(1000)
+
+        print("Введите код для активации: ")
+        enteredCode = readln().toInt()
     }
-    println("Доброй пожаловать!")
+    println("Добро пожаловать!")
+
+
+//    do {
+//        val activationCode = (1000 until 10000).random()
+//
+//        println("Ваш код авторизации: $activationCode")
+//        Thread.sleep(1000)
+//
+//        print("Введите код для авторизации: ")
+//        val enteredCode = readln().toInt()
+//
+//    } while (enteredCode != activationCode)
+//    println("Доброй пожаловать!")
 }
