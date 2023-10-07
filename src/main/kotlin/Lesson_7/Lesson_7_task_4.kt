65a216576512000aefefbf033b3b660d643a2fb0
muhck <120320336+muhck@users.noreply.github.com>
2023-10-07 11:05:51 +0400

    KS-7-4 написал таймер (#41)
    * KS-7-4 написал таймер
    
    * KS-7-4 убрал условие

--- src/main/kotlin/Lesson_7/Lesson_7_task_4.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_task_4.kt
@@ -1,2 +1,12 @@
 package Lesson_7
 
+fun main() {
+    print("Введите количество секунд которые нужно засечь: ")
+    val enteredNumber = readln().toInt()
+
+    for (i in enteredNumber downTo 1) {
+        println("Осталось секунд: $i")
+        Thread.sleep(1000)
+    }
+    print("Время вышло!")
+}
