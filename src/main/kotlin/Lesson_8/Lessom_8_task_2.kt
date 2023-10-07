9277b202cb05e8193b205a06d71d6fff8df494a9
muhck <dimamic@yandex.ru>
2023-10-07 12:36:07 +0400

    KS-8-2 поиск ингредиента в массиве

--- src/main/kotlin/Lesson_8/Lessom_8_task_2.kt
+++ src/main/kotlin/Lesson_8/Lessom_8_task_2.kt
@@ -1,2 +1,18 @@
 package Lesson_8
 
+fun main() {
+    val arrayIngredients = arrayOf("колбаса", "яйцо", "горошек", "огурцы", "картошка", "морковь", "майонез")
+    print("Напиши название ингредиента, который необходимо найти: ")
+    val enteredIngredient = readln()
+    var found = false
+
+    for (i in arrayIngredients.indices) {
+        if (arrayIngredients[i] == enteredIngredient) {
+            found = true
+            break
+        }
+    }
+
+    if (found) println("Ингредиент $enteredIngredient в рецепте есть")
+    else println("Ингредиента $enteredIngredient в рецепте нет")
+}
