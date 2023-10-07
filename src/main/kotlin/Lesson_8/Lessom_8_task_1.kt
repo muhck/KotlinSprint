7482e94a46a4e8a4ddac75ce1458280dde156775
muhck <120320336+muhck@users.noreply.github.com>
2023-10-08 00:54:18 +0400

    KS-8-1 общее число из массива (#44)

--- src/main/kotlin/Lesson_8/Lessom_8_task_1.kt
+++ src/main/kotlin/Lesson_8/Lessom_8_task_1.kt
@@ -1,2 +1,13 @@
 package Lesson_8
 
+fun main() {
+    val arrayWeek = arrayOf("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье")
+    val arrayOfViews = arrayOf(35, 28, 31, 25, 32, 57, 44)
+
+    for ((i, d) in arrayWeek.withIndex()) {
+        println("Просмотров в $d: ${arrayOfViews[i]}")
+        Thread.sleep(500)
+    }
+
+    println("\nВсего просмотров за неделю: ${arrayOfViews.sum()}")
+}
