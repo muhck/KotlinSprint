53d6e2911ea553341cbe071f59471960e6409ad5
muhck <dimamic@yandex.ru>
2023-10-07 14:47:19 +0400

    KS-8-5 создаем массив из ингредиентов

--- src/main/kotlin/Lesson_8/Lessom_8_task_5.kt
+++ src/main/kotlin/Lesson_8/Lessom_8_task_5.kt
@@ -1,2 +1,16 @@
 package Lesson_8
 
+fun main() {
+    print("Дайте наименование блюда: ")
+    val nameFood = readln()
+
+    var arrayIngredients: Array<String> = arrayOf()
+    print("Отлично! Напишите количество планируемых ингредиентов: ")
+    val numberIngr = readln().toInt()
+
+    for (i in 1..numberIngr) {
+        print("Напишите $i ингредиент: ")
+        arrayIngredients += readln()
+    }
+    println("Вот ваш список ингредиентов для блюда \"$nameFood\": ${arrayIngredients.joinToString(", ")}")
+}
