d4992baf5fbac23a5c1515da2bf7c170758ccb19
muhck <120320336+muhck@users.noreply.github.com>
2023-10-08 20:40:02 +0400

    KS-8-3 поиск ингредиента в массиве без цикла (#46)

--- src/main/kotlin/Lesson_8/Lessom_8_task_3.kt
+++ src/main/kotlin/Lesson_8/Lessom_8_task_3.kt
@@ -1,2 +1,11 @@
 package Lesson_8
 
+fun main() {
+    val arrayIngredients = arrayOf("колбаса", "яйцо", "горошек", "огурцы", "картошка", "морковь", "майонез")
+    print("Напиши название ингредиента, который необходимо найти: ")
+    val enteredIngredient = readln()
+    val found = enteredIngredient in arrayIngredients
+
+    if (found) println("Ингредиент $enteredIngredient в рецепте есть")
+    else println("Ингредиента $enteredIngredient в рецепте нет")
+}
