7525befe2b7f5b2dec583bdf9b6f6781d6efd9c4
muhck <120320336+muhck@users.noreply.github.com>
2023-10-09 21:11:38 +0400

    KS-8-5 создаем массив из ингредиентов (#48)
    * KS-8-5 создаем массив из ингредиентов
    
    * KS-8-5 исправил цикл, создал массив с указанным кол. с консоли

--- src/main/kotlin/Lesson_8/Lessom_8_task_5.kt
+++ src/main/kotlin/Lesson_8/Lessom_8_task_5.kt
@@ -1,2 +1,16 @@
 package Lesson_8
 
+fun main() {
+    print("Дайте наименование блюда: ")
+    val nameFood = readln()
+
+    print("Отлично! Напишите количество планируемых ингредиентов: ")
+    val numberIngr = readln().toInt()
+    val arrayIngr = arrayOfNulls<String>(numberIngr)
+
+    for (index in 1..numberIngr) {
+        print("Напишите $index ингредиент: ")
+        arrayIngr[index - 1] = readln()
+    }
+    println("Вот ваш список ингредиентов для блюда \"$nameFood\": ${arrayIngr.joinToString(", ")}")
+}
