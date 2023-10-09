7a50f380c0236cc152f34b0973a5a44151ed0ce3
muhck <dimamic@yandex.ru>
2023-10-10 00:59:51 +0400

    KS-9-3 рассчитываем кол-во ингредиентов

--- src/main/kotlin/Lesson_9/Lesson_9_task_3.kt
+++ src/main/kotlin/Lesson_9/Lesson_9_task_3.kt
@@ -1,2 +1,15 @@
 package Lesson_9
 
+fun main() {
+    val listOneDish = listOf(2, 50, 15)
+
+    print("Введите количество блюд для приготовления: ")
+    val numberOfDishes = readln().toInt()
+
+    val newListDish = listOneDish.map { it * numberOfDishes }
+
+    println(
+        "На $numberOfDishes порций вам понадобится: яиц-${newListDish[0]}, молока-${newListDish[1]}," +
+                " сливочного масла-${newListDish[2]}."
+    )
+}
