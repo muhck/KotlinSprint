ab6d73138644895265fccfe076b5036615f9b3e6
muhck <dimamic@yandex.ru>
2023-10-09 22:29:49 +0400

    KS-9-1 вывод списка ингредиентов

--- src/main/kotlin/Lesson_9/Lesson_9_task_1.kt
+++ src/main/kotlin/Lesson_9/Lesson_9_task_1.kt
@@ -1,2 +1,8 @@
 package Lesson_9
 
+fun main() {
+    val listIngredients = listOf("колбаса", "яйцо", "горошек", "огурцы", "картошка", "морковь", "майонез")
+
+    println("В рецепте есть следующие ингредиенты:")
+    listIngredients.forEach { println(it) }
+}
