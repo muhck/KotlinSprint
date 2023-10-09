3407712cff76c0a8f65b65aca466d5d9bcc53dd1
muhck <120320336+muhck@users.noreply.github.com>
2023-10-09 21:04:15 +0400

    KS-8-4 поиск и изменение ингредиента в массиве (#47)
    * KS-8-4 поиск и изменение ингредиента в массиве
    
    * KS-8-4 убрал цикл

--- src/main/kotlin/Lesson_8/Lessom_8_task_4.kt
+++ src/main/kotlin/Lesson_8/Lessom_8_task_4.kt
@@ -1,2 +1,18 @@
 package Lesson_8
 
+fun main() {
+    val arrayIngredients = arrayOf("колбаса", "яйцо", "горошек", "огурцы", "картошка", "морковь", "майонез")
+    println("Вот список ингредиентов для салата оливье: ${arrayIngredients.joinToString(", ")}")
+    print("Напиши название ингредиента, который необходимо заменить: ")
+    val enteredIngredient = readln()
+    val found = enteredIngredient in arrayIngredients
+
+    if (!found) println("Ингредиента $enteredIngredient в рецепте нет")
+    else {
+        print("Ингредиент $enteredIngredient заменить на: ")
+        val newIngredient = readln()
+        val indexIngredient = arrayIngredients.indexOf(enteredIngredient)
+        arrayIngredients[indexIngredient] = newIngredient
+        println("Готово! Вы сохранили следующий список: ${arrayIngredients.joinToString(", ")}")
+    }
+}
