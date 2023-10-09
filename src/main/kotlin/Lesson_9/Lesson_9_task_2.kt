a9689bed33d4f18018ccb917eb19f1fa72f46b56
muhck <dimamic@yandex.ru>
2023-10-10 00:37:57 +0400

    KS-9-2 список ингредиентов с возможность добавления

--- src/main/kotlin/Lesson_9/Lesson_9_task_2.kt
+++ src/main/kotlin/Lesson_9/Lesson_9_task_2.kt
@@ -1,2 +1,16 @@
 package Lesson_9
 
+fun main() {
+    val listIngr = mutableListOf("курица", "морковь", "картофель")
+    println("В рецепте есть базовые ингредиенты: ${listIngr.joinToString(", ")}")
+
+    print("Желаете добавить еще? Ваш ответ: ")
+    val answer = readln()
+
+    if (answer == "да") {
+        print("Какой ингредиент вы хотите добавить? Ваш ответ: ")
+        listIngr.add(readln())
+    } else return
+
+    println("Теперь в рецепте есть следующие ингредиенты: ${listIngr.joinToString(", ")}")
+}
