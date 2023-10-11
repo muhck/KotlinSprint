a336c513d6b9d754d3ffa632dbfac2b3b0419a8d
muhck <120320336+muhck@users.noreply.github.com>
2023-10-11 18:15:58 +0400

    KS-9-4 создаем список с одной строки (#52)

--- src/main/kotlin/Lesson_9/Lesson_9_task_4.kt
+++ src/main/kotlin/Lesson_9/Lesson_9_task_4.kt
@@ -1,2 +1,9 @@
 package Lesson_9
 
+fun main() {
+    print("Напишите пять игредиентов для своего блюда через запятую: ")
+    val listIngr = readln().split(", ")
+
+    val newListIngr = listIngr.sorted()
+    println("Вот ваш список ингредиентов в алфавитном порядке: ${newListIngr.joinToString(", ")}")
+}
