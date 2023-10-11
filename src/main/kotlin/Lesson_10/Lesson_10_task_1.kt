1d93e6d9457749df84b267b33fc7091b6f2137c3
muhck <dimamic@yandex.ru>
2023-10-12 02:07:17 +0400

    KS-10-1 игра в кости

--- src/main/kotlin/Lesson_10/Lesson_10_task_1.kt
+++ src/main/kotlin/Lesson_10/Lesson_10_task_1.kt
@@ -1,2 +1,24 @@
 package Lesson_10
 
+fun main() {
+    var playerPoints: Int
+    var computerPoints: Int
+    do {
+        playerPoints = randomPoints()
+        computerPoints = randomPoints()
+        println("Первым ходит игрок, бросая кости, выпадает: $playerPoints")
+        Thread.sleep(1000)
+        println("Вторым ходит компьютер, бросая кости, выпадает: $computerPoints")
+        Thread.sleep(1000)
+
+        if (playerPoints == computerPoints) {
+            println("Ничья? Переиграем.")
+            Thread.sleep(1000)
+        }
+    } while (playerPoints == computerPoints)
+
+    if (playerPoints > computerPoints) print("Победило человечество.")
+    else print("Победил компьютер.")
+}
+
+fun randomPoints() = (1..6).random()
