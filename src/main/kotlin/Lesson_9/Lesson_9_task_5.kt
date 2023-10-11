8d48b7a4f525fdec7f86963c2126df603e063baf
muhck <dimamic@yandex.ru>
2023-10-11 05:48:38 +0400

    KS-9-5 создаем список без одинаковых эл-в и выводом с заглавной буквы через запятую

--- src/main/kotlin/Lesson_9/Lesson_9_task_5.kt
+++ src/main/kotlin/Lesson_9/Lesson_9_task_5.kt
@@ -1,2 +1,27 @@
 package Lesson_9
 
+import java.util.*
+
+fun main() {
+    val listIngr: MutableList<String> = mutableListOf()
+    println("Напишите пять игредиентов для своего блюда.")
+
+    for (i in 1..5) {
+        print("$i ингредиент: ")
+        var ingr = readln()
+
+        while (listIngr.contains(ingr)) {
+            println("Ингредиент $ingr уже есть в списке. Попробуй ввести другой: ")
+            ingr = readln()
+        }
+        listIngr.add(ingr)
+    }
+
+    listIngr.sort()
+    println(
+        "Вот ваш список ингредиентов  в алфавитном порядке: ${
+            listIngr.joinToString(", ")
+                .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+        }"
+    )
+}
