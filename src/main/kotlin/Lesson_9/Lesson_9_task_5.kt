458e7819946b7d1958cbf26f14fe9efcb984f2ff
muhck <dimamic@yandex.ru>
2023-10-11 19:43:37 +0400

    KS-9-5 изменил коллекцию на "set". Убрал цикл

--- src/main/kotlin/Lesson_9/Lesson_9_task_5.kt
+++ src/main/kotlin/Lesson_9/Lesson_9_task_5.kt
@@ -3,24 +3,19 @@ package Lesson_9
 import java.util.*
 
 fun main() {
-    val listIngr: MutableList<String> = mutableListOf()
+    val listIngr: MutableSet<String> = mutableSetOf()
     println("Напишите пять игредиентов для своего блюда.")
 
     for (i in 1..5) {
         print("$i ингредиент: ")
-        var ingr = readln()
-
-        while (listIngr.contains(ingr)) {
-            println("Ингредиент $ingr уже есть в списке. Попробуй ввести другой: ")
-            ingr = readln()
-        }
+        val ingr = readln()
         listIngr.add(ingr)
     }
 
-    listIngr.sort()
+    val newListIngr = listIngr.sorted()
     println(
         "Вот ваш список ингредиентов  в алфавитном порядке: ${
-            listIngr.joinToString(", ")
+            newListIngr.joinToString(", ")
                 .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
         }"
     )
