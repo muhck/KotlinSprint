43c09e920aa89c14b9f9d2d42d9c8a157fbb5add
muhck <dimamic@yandex.ru>
2023-10-12 03:20:00 +0400

    KS-10-2 проверка длины функцией

--- src/main/kotlin/Lesson_10/Lesson_10_task_2.kt
+++ src/main/kotlin/Lesson_10/Lesson_10_task_2.kt
@@ -1,2 +1,17 @@
 package Lesson_10
 
+fun main() {
+    println("Для регистрации в приложении придумайте логин и пароль. Они должны содержать минимум 4 символа.")
+    Thread.sleep(1000)
+    print("Сперва придумайте логин: ")
+    val userLogin = readln()
+    print("Теперь придумайте пароль: ")
+    val userPassword = readln()
+
+    lengthChek(userLogin, userPassword)
+}
+
+fun lengthChek(aa: String, bb: String) {
+    if (aa.length < 4 || bb.length < 4) println("Логин или пароль недостаточно длинные.")
+    else println("Добро пожаловать!")
+}
