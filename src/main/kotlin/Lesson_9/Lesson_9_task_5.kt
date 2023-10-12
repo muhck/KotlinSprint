edbe233adcaa055c3e88373018a7a11c28a122db
muhck <120320336+muhck@users.noreply.github.com>
2023-10-12 19:00:16 +0400

    KS-9-5 создаем список без одинаковых эл-в и выводом с заглавной буквы через запятую (#53)
    * KS-9-5 создаем список без одинаковых эл-в и выводом с заглавной буквы через запятую
    
    * KS-9-5 изменил коллекцию на "set". Убрал цикл
    
    * KS-9-5 изменил коллекцию на "set". Убрал цикл

--- src/main/kotlin/Lesson_9/Lesson_9_task_5.kt
+++ src/main/kotlin/Lesson_9/Lesson_9_task_5.kt
@@ -1,2 +1,21 @@
 package Lesson_9
 
+import java.util.*
+
+fun main() {
+    val listIngr: MutableSet<String> = mutableSetOf()
+    println("Напишите пять игредиентов для своего блюда.")
+
+    for (i in 1..5) {
+        print("$i ингредиент: ")
+        val ingr = readln()
+        listIngr.add(ingr)
+    }
+
+    val newListIngr = listIngr.sorted()
+    println(
+        "Вот ваш список ингредиентов  в алфавитном порядке: ${
+            newListIngr.joinToString(", ").replaceFirstChar { it.titlecase(Locale.getDefault()) }
+        }"
+    )
+}
