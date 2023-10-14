18ff7e436a2e5926605577ba2fbb0ad8ded3c2ad
muhck <120320336+muhck@users.noreply.github.com>
2023-10-14 17:47:06 +0400

    KS-10-2 проверка длины функцией (#56)
    * KS-10-2 проверка длины функцией
    
    * KS-10-2 вынес минимальную длину строки в константу

--- src/main/kotlin/Lesson_10/Lesson_10_task_2.kt
+++ src/main/kotlin/Lesson_10/Lesson_10_task_2.kt
@@ -1,2 +1,19 @@
 package Lesson_10
 
+const val MINIMUM_STRING_LENGHT = 4
+
+fun main() {
+    println("Для регистрации в приложении придумайте логин и пароль. Они должны содержать минимум 4 символа.")
+    print("Сперва придумайте логин: ")
+    val userLogin = readln()
+    print("Теперь придумайте пароль: ")
+    val userPassword = readln()
+
+    lengthChek(userLogin, userPassword)
+}
+
+fun lengthChek(login: String, password: String) {
+    if (login.length < MINIMUM_STRING_LENGHT || password.length < MINIMUM_STRING_LENGHT)
+        println("Логин или пароль недостаточно длинные.")
+    else println("Добро пожаловать!")
+}
