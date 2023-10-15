a0c11daa12102496a60cc50dfc5eede791d86f46
muhck <120320336+muhck@users.noreply.github.com>
2023-10-15 21:34:49 +0400

    KS-10-3 рандомный пароль через функцию (#57)
    * KS-10-3 рандомный пароль через функцию
    
    * KS-10-3 объединил циклы, дал названия параметрам, поменял диапазон символов

--- src/main/kotlin/Lesson_10/Lesson_10_task_3.kt
+++ src/main/kotlin/Lesson_10/Lesson_10_task_3.kt
@@ -1,2 +1,21 @@
 package Lesson_10
 
+fun main() {
+    print("Чтобы задать длину сгенерированного пароля, введите необходимое количество символов: ")
+    val enteredNumber = readln().toInt()
+
+    print("Вот ваш пароль: ")
+    generatePassword(enteredNumber)
+}
+
+fun generatePassword(passwordLength: Int) {
+    val password = arrayOfNulls<Char>(passwordLength)
+
+    for (index in 1..passwordLength) {
+        when (index % 2) {
+            1 -> password[index - 1] = ('0'..'9').random()
+            0 -> password[index - 1] = (32..47).random().toChar()
+        }
+    }
+    return password.forEach { print(it) }
+}
