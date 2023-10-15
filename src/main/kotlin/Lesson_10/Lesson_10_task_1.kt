ccb84005dfac4ebb2beefb97786847131dd63de8
muhck <120320336+muhck@users.noreply.github.com>
2023-10-15 22:23:44 +0400

    KS-10-1 игра в кости (#54)
    * KS-10-1 игра в кости
    
    * KS-10-1 перенес условие и переменные в цикл

--- src/main/kotlin/Lesson_10/Lesson_10_task_1.kt
+++ src/main/kotlin/Lesson_10/Lesson_10_task_1.kt
@@ -1,2 +1,25 @@
 package Lesson_10
 
+fun main() {
+    do {
+        val playerPoints = randomPoints()
+        val computerPoints = randomPoints()
+        println("Первым ходит игрок, бросая кости, выпадает: $playerPoints")
+        Thread.sleep(1000)
+        println("Вторым ходит компьютер, бросая кости, выпадает: $computerPoints")
+        Thread.sleep(1000)
+
+        if (playerPoints < computerPoints) {
+            print("Победил компьютер.")
+
+        } else if (playerPoints > computerPoints) {
+            print("Победило человечество.")
+
+        } else {
+            println("Ничья? Переиграем.")
+            Thread.sleep(1000)
+        }
+    } while (playerPoints == computerPoints)
+}
+
+fun randomPoints() = (1..6).random()
