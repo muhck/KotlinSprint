da6ea1ac2ac0dcc228304b780915695322a4e93c
muhck <dimamic@yandex.ru>
2023-10-17 20:21:30 +0400

    KS-10-5 вывод списка товара по токену

--- src/main/kotlin/Lesson_10/Lesson_10_task_5.kt
+++ src/main/kotlin/Lesson_10/Lesson_10_task_5.kt
@@ -1,2 +1,21 @@
 package Lesson_10
 
+const val LOGIN_USER = "koshka_moshka"
+const val PASSWORD_USER = "12345678"
+val BASKET_OF_PRODUCTS = arrayOf("кровать", "матрас", "набор инструментов", "картина", "комод", "скотч")
+fun main() {
+    print("Введите логин для входа: ")
+    val loginInput = readln()
+    print("Введите пароль для входа: ")
+    val passwordInput = readln()
+
+    if (generateToken(loginInput, passwordInput) != null) {
+        println("Вот ваш список покупок: ${BASKET_OF_PRODUCTS.joinToString(", ")}.")
+    } else println("Неправильно введен логин или пароль.")
+}
+
+fun generateToken(login: String, password: String): String? {
+    var token: String? = null
+    if (login == LOGIN_USER && password == PASSWORD_USER) token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpX"
+    return token
+}
