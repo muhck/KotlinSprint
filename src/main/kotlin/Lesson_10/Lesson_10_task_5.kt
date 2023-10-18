d0ebc2030dcc13501a357a168ffbdb701502bc37
muhck <dimamic@yandex.ru>
2023-10-18 16:57:29 +0400

    KS-10-5 перенес массив в main

--- src/main/kotlin/Lesson_10/Lesson_10_task_5.kt
+++ src/main/kotlin/Lesson_10/Lesson_10_task_5.kt
@@ -2,15 +2,16 @@ package Lesson_10
 
 const val LOGIN_USER = "koshka_moshka"
 const val PASSWORD_USER = "12345678"
-val BASKET_OF_PRODUCTS = arrayOf("кровать", "матрас", "набор инструментов", "картина", "комод", "скотч")
 fun main() {
+    val basketOfProducts = arrayOf("кровать", "матрас", "набор инструментов", "картина", "комод", "скотч")
+
     print("Введите логин для входа: ")
     val loginInput = readln()
     print("Введите пароль для входа: ")
     val passwordInput = readln()
 
     if (generateToken(loginInput, passwordInput) != null) {
-        println("Вот ваш список покупок: ${BASKET_OF_PRODUCTS.joinToString(", ")}.")
+        println("Вот ваш список покупок: ${basketOfProducts.joinToString(", ")}.")
     } else println("Неправильно введен логин или пароль.")
 }
 
