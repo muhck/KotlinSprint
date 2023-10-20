c45e4e53d99efc9d237b1bf50a212c0088cb74f7
muhck <120320336+muhck@users.noreply.github.com>
2023-10-20 23:21:27 +0400

    KS-10-4 сделал игру с возможностью продолжения (#58)
    * KS-10-4 сделал игру с возможностью продолжения
    
    * KS-10-4 поменял имя функций, вывел константу, убрал 1 функцию
    
    * KS-10-4 создал массив с возможными вариантами ответа
    
    * KS-10-4 вывел константу и добавил lowercase

--- src/main/kotlin/Lesson_10/Lesson_10_task_4.kt
+++ src/main/kotlin/Lesson_10/Lesson_10_task_4.kt
@@ -1,2 +1,40 @@
 package Lesson_10
 
+const val CONTINUE_GAME = "да"
+
+fun main() {
+    var numberWinsPlayer = 0
+    do {
+        val winPlayer = appointWinner()
+        if (winPlayer) numberWinsPlayer++
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        val continueGame = readln().lowercase()
+    } while (continueGame == CONTINUE_GAME)
+    printNumberOfWins(numberWinsPlayer)
+}
+
+fun appointWinner(): Boolean {
+    var winPlayer = false
+    do {
+        val playerPoints = (1..6).random()
+        val computerPoints = (1..6).random()
+        println("Первым ходит игрок, бросая кости, выпадает: $playerPoints")
+        Thread.sleep(1000)
+        println("Вторым ходит компьютер, бросая кости, выпадает: $computerPoints")
+        Thread.sleep(1000)
+
+        if (playerPoints < computerPoints) {
+            println("Победил компьютер.")
+
+        } else if (playerPoints > computerPoints) {
+            winPlayer = true
+            println("Победило человечество.")
+        } else {
+            println("Ничья? Переиграем.")
+            Thread.sleep(1000)
+        }
+    } while (playerPoints == computerPoints)
+    return winPlayer
+}
+
+fun printNumberOfWins(number: Int) = print("Количество побед у человека: $number")
